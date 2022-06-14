5b0b816d6459a09820a0783b4c06e560de0128d7
Marvin Steckel <themarvdoteu@gmail.com>
2022-06-14 10:37:08 +0200

    Implement quill-react

--- components/Blogpost.jsx
+++ components/Blogpost.jsx
@@ -1,8 +1,11 @@
-import { Grid, Typography } from '@mui/material';
+import Link from 'next/link';
 import { format } from 'date-fns';
+import styled from '@emotion/styled';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 export default function Blogpost({ post, slice = false }) {
-  const { title, content, created } = post;
+  const { title, content, created, id } = post;
 
   return (
     <Grid
@@ -13,20 +16,29 @@ export default function Blogpost({ post, slice = false }) {
       direction="column"
     >
       <Grid item textAlign="right">
-        <Typography variant="h3" component="h1">
-          {title}
+        <Typography variant="h3" component={slice ? 'h2' : 'h1'}>
+          <Link href={`/blog/${id}`}>{title}</Link>
         </Typography>
         <Typography variant="caption">
           {format(new Date(created), 'MM/dd/yyyy hh:mm')}
         </Typography>
       </Grid>
       <Grid item>
-        <Typography paragraph={true}>
-          {slice
-            ? `${content.slice(0, 200)}${content.length > 200 ? '…' : ''}`
-            : content}
-        </Typography>
+        <StyledContent
+          slice={slice}
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
       </Grid>
     </Grid>
   );
 }
+
+const StyledContent = styled.div`
+  ${({ slice }) =>
+    slice &&
+    `
+    max-height: 4ch;
+    overflow: hidden;
+    margin-bottom: 2rem;
+  `}
+`;
---
+++ components/Richtext.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import styled from '@emotion/styled';
+
+import 'react-quill/dist/quill.snow.css';
+const ReactQuill = dynamic(import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading Editor</p>,
+});
+
+const editorHeight = 300;
+
+export default function Richtext({ value, onChange }) {
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [
+          { list: 'ordered' },
+          { list: 'bullet' },
+          { indent: '-1' },
+          { indent: '+1' },
+        ],
+        ['link'],
+        ['clean'],
+      ],
+    },
+  };
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+  ];
+
+  return (
+    <StyledEditor>
+      <ReactQuill
+        formats={formats}
+        modules={modules}
+        value={value}
+        onChange={onChange}
+        style={{ height: editorHeight - 44 }}
+      />
+    </StyledEditor>
+  );
+}
+
+const StyledEditor = styled.div`
+  height: ${editorHeight}px;
+  .quill > .ql-toolbar:first-child {
+    display: none !important;
+  }
+`;
